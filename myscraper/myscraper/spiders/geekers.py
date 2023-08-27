5b9b240bb7f36eafa048277a29ec54e59f4255a5
Jorge R <jorge.retamal@zweicom.com>
2023-08-26 23:30:46 -0400

    chore(spider):geekers

---
+++ myscraper/myscraper/spiders/geekers.py
@@ -0,0 +1,26 @@
+import scrapy
+
+
+class EntreJuegos(scrapy.Spider):
+    name = "geekers"
+    allowed_domains = ["geekers.cl"]
+    start_urls = ["https://www.geekers.cl/pokemon-tcg"]
+
+    def parse(self, response):
+        products = response.css('.product-block')
+
+        for product in products:
+            yield{
+                'link': product.css('.product-block__caption-info a::attr(href)').get(),
+                'img': product.css('img::attr(src)').get(),
+                'nombre': product.css('.product-block__name::text').get(),
+                'stock_label': product.css('.product-block__status::text').get(),
+                'precio': product.css('.product-block__price_value::text').get(),
+            }
+        
+        # Siguientes páginas
+        next_page = response.css('li.next a::attr(href)').get()
+        if next_page is not None:
+            yield response.follow(next_page, self.parse)
+
+# El stock se puede saber del max input
