8538209441683484f51496c2708ea153df09b701
Jorge R <jorge.retamal@zweicom.com>
2023-09-17 22:34:37 -0300

    chore(api): Se mejora el código

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, jsonify
-import redis
+"""This module contains the Principal APP API."""
+
 import os
+import redis
+from flask import Flask, jsonify
+# from itertools import groupby
 
 app = Flask(__name__)
 
@@ -12,33 +15,52 @@ r = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, password=REDIS_PASS, decode_re
 
 @app.route('/get_products', methods=['GET'])
 def get_products():
-    # Utiliza el comando KEYS para encontrar todas las keys que coinciden con el patrón "*:product:*"
+    """This function gets all products from Redis"""
+
+    # Obtener todas las claves que coincidan con el patrón '*:product:*'
     all_keys = r.keys('*:product:*')
-    
-    # Extrae los nombres de las tiendas únicas desde las keys
+    # Extraer los nombres de las tiendas únicas desde las claves
     product_stores = list(set(key.split(":")[0] for key in all_keys))
-    
     response_data = []
 
+    # Crear un pipeline Redis
+    pipeline = r.pipeline()
+
+    # Precargar todos los hgetall en el pipeline
+    for store in product_stores:
+        i = 1
+        while True:
+            product_key = f"{store}:product:{i}"
+            if product_key in all_keys:
+                pipeline.hgetall(product_key)
+                i += 1
+            else:
+                break
+
+    # Ejecutar todos los comandos en el pipeline en una sola ronda
+    results = pipeline.execute()
+
+    # Procesar los resultados
+    result_index = 0
     for store in product_stores:
         store_products = []
         i = 1
         while True:
             product_key = f"{store}:product:{i}"
-            product_data = r.hgetall(product_key)
-            
-            if not product_data:
+            if product_key in all_keys:
+                product_data = results[result_index]
+                result_index += 1
+
+                store_products.append({
+                    'product_link': product_data.get('product_link', ''),
+                    'product_name': product_data.get('product_name', ''),
+                    'product_image': product_data.get('product_image', ''),
+                    'product_available_label': product_data.get('product_available_label', ''),
+                    'product_price': product_data.get('product_price', '')
+                })
+                i += 1
+            else:
                 break
-
-            store_products.append({
-                'product_link': product_data.get('product_link', ''),
-                'product_name': product_data.get('product_name', ''),
-                'product_image': product_data.get('product_image', ''),
-                'product_available_label': product_data.get('product_available_label', ''),
-                'product_price': product_data.get('product_price', '')
-            })
-            i += 1
-
         response_data.append({store: store_products})
 
     return jsonify(response_data)
@@ -46,17 +68,45 @@ def get_products():
 if __name__ == '__main__':
     app.run(debug=True)
 
+
+# def get_products():
+#     """This class defines get all products redis"""
+#     # Usa SCAN en lugar de KEYS para evitar bloquear el servidor de Redis
+#     cursor = '0'
+#     all_keys = []
+#     while cursor != 0:
+#         cursor, keys = r.scan(cursor=cursor, match='*:product:*', count=1000)
+#         all_keys.extend(keys)
+#     # Ordena y agrupa las claves por tienda
+#     sorted_keys = sorted(all_keys, key=lambda x: x.split(":")[0])
+#     grouped_keys = groupby(sorted_keys, key=lambda x: x.split(":")[0])
+#     response_data = []
+
+#     for store, keys in grouped_keys:
+#         store_products = []
+#         # Realiza una sola llamada a hgetall para cada grupo de productos de una tienda
+#         for product_key in keys:
+#             product_data = r.hgetall(product_key)
+#             if product_data:
+#                 store_products.append({
+#                     'product_link': product_data.get('product_link', ''),
+#                     'product_name': product_data.get('product_name', ''),
+#                     'product_image': product_data.get('product_image', ''),
+#                     'product_available_label': product_data.get('product_available_label', ''),
+#                     'product_price': product_data.get('product_price', '')
+#                 })
+#         response_data.append({store: store_products})
+#     return jsonify(response_data)
 # def get_products():
 #     product_stores = ['carduniverse', 'araucaniagaming']
 #     response_data = []
-
 #     for store in product_stores:
 #         store_products = []
 #         i = 1
 #         while True:
 #             product_key = f"{store}:product:{i}"
 #             product_data = r.hgetall(product_key)
-            
+
 #             if not product_data:
 #                 break
 
@@ -75,4 +125,3 @@ if __name__ == '__main__':
 
 # if __name__ == '__main__':
 #     app.run(debug=True)
-
